53e27d62f61e573e6fdfc6f3e05fc0633bd0712b
Yousen Zhang <yzhang11@bnl.gov>
2023-11-14 19:13:58 -0500

    create a few data list

--- analysis.py
+++ analysis.py
@@ -18,6 +18,9 @@ class HistFactory:
     """
 
     def __init__(self):
+        # static member?
+        self.__mpion = 139 # MeV
+        self.__filter_pdgid = 211
         self.__flist = []
         self.__tname = "sim"
         # self.__info_tname = "info"
@@ -93,12 +96,76 @@ class HistFactory:
                                          subt=self.__track_tname)
                                  for f in self.__flist])
         for batch in f_iter:
-            self.__miniloop(batch)
+            # self.__miniloop(batch.keys())
+            # select pions
+            pdgid_mask = batch["track.pdgid"] == self.__filter_pdgid
+            selected = batch[pdgid_mask]
+            selected_pdgid = batch[pdgid_mask]['track.pdgid']
+            # from here, I always assume one particle in one event
+            print(selected_pdgid[:, 0])
 
-    @staticmethod
-    @nb.njit
-    def __miniloop(batch):
-        """loop over a batch
-        """
-        for event in batch:
-            print(event)
+            # analyzed = nevent * [
+            # nstep *
+            # [dE, dE/dz, dE/dr, dz, px, py, pz, x, y, z, Ek, volume],
+            # z_stop, init_px, init_py, init_pz, init_Ek, init_p]
+
+            # [ievent, iparticle, isteps]
+            steps = {}
+            for d in ["x", "y", "z"]:
+                pre_loc = selected["track.post_{}".format(d)][:, 0, 0:-1]
+                pos_loc = selected["track.post_{}".format(d)][:, 0, 1:]
+                steps["diff_{}".format(d)] = pos_loc - pre_loc
+                steps["pos_{}".format(d)] = pos_loc
+
+                steps["pos_p{}".format(d)] = selected[
+                    "track.post_p{}".format(d)][:, 0, 1:]
+
+                steps["init_p{}".format(d)] = selected[
+                    "track.post_p{}".format(d)][:, 0, 0]
+
+            steps["diff_r"] = np.sqrt(
+                steps["diff_x"]**2 + steps["diff_y"]**2
+                + steps["diff_z"]**2
+            )
+
+            steps["pos_KE"] = np.sqrt(
+                steps["pos_px"]**2 + steps["pos_py"]**2
+                + steps["pos_pz"]**2 + self.__mpion**2
+            ) - self.__mpion
+
+            steps["init_KE"] = np.sqrt(
+                steps["init_px"]**2 + steps["init_py"]**2
+                + steps["init_pz"]**2 + self.__mpion**2
+            ) - self.__mpion
+
+            steps["init_p"] = np.sqrt(
+                steps["init_px"]**2 + steps["init_py"]**2
+                + steps["init_pz"]**2
+            )
+
+            steps["volume"] = selected["track.volume"][:, 0, 1:]
+            steps["dE"] = selected["track.edep"][:, 0, 1:]
+
+            # not sure how to deal with dz=0
+            # steps["dE/dz"] = steps["dE"]/steps["diff_z"]
+            steps["z_stop"] = selected["track.post_z"][:, 0, -1]
+
+            analyzed = ak.Array(steps)
+            # print(analyzed)
+            # for event in analyzed:
+            #     print(event.tolist())
+
+    # @staticmethod
+    # @nb.njit
+    # def __miniloop(batch):
+    #     """loop over a batch
+    #     """
+    #     for event in batch:
+    #         self.__single_event(event)
+
+    # @staticmethod
+    # @nb.njit
+    # def __single_event(event):
+    #     """Process single event
+    #     """
+    #     return
