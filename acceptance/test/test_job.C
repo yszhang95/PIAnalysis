8871a939e8052830526a1830a4e0398120891199
Yousen Zhang <yzhang11@bnl.gov>
2024-04-16 10:49:13 -0400

    some changes

--- acceptance/test/test_job.C
+++ acceptance/test/test_job.C
@@ -63,12 +63,12 @@ void run_test_pimue()
   pi.treename("sim");
   pi.add_file("pimunu-test.root");
   pi.out_file("pimunu-job-output.root");
-  // std::unique_ptr<PIAna::PIEvtNbFilter> evtnbfilter =
-  //     std::make_unique<PIAna::PIEvtNbFilter>("EvtNb", 0);
-  // evtnbfilter->load_filter("0:4105:4105,0:9805:9805");
-  // pi.add_action("EvtNb", std::move(evtnbfilter));
-  // auto evtnbfilterptr = dynamic_cast<PIAna::PIEvtNbFilter*>(pi.get_action("EvtNb"));
-  // evtnbfilterptr->load_filter("0:9599:9599");
+  std::unique_ptr<PIAna::PIEvtNbFilter> evtnbfilter =
+      std::make_unique<PIAna::PIEvtNbFilter>("EvtNb", 0);
+  evtnbfilter->load_filter("0:4105:4105,0:9805:9805");
+  pi.add_action("EvtNb", std::move(evtnbfilter));
+  auto evtnbfilterptr = dynamic_cast<PIAna::PIEvtNbFilter*>(pi.get_action("EvtNb"));
+  evtnbfilterptr->load_filter("0:9599:9599");
   pi.add_action("PiDAR",
                 std::make_unique<PIAna::PIPiDARFilter>("PiDAR", static_cast<int>(PIAna::EvtCode::PiDAR)));
   pi.add_action("DecPos", std::make_unique<PIAna::PITrueDecPos>("DecPos"));
@@ -77,14 +77,15 @@ void run_test_pimue()
   pi.add_action("RecHits", std::make_unique<PIAna::PIHitProducer>("RecHits"));
   auto rechits = dynamic_cast<PIAna::PIHitProducer *>(pi.get_action("RecHits"));
   rechits->verbose(true);
+  rechits->de_thres(0.0155);
   pi.add_action("Topo", std::make_unique<PIAna::PITopoProducer>("Topo"));
   auto topoproducer =
       dynamic_cast<PIAna::PITopoProducer *>(pi.get_action("Topo"));
   topoproducer->min_tcluster(3);
-  // topoproducer->verbose(true);
-  // std::unique_ptr<PIAna::PITreeAnalyzer> treeanalyzer =
-  //     std::make_unique<PIAna::PITreeAnalyzer>("AnaHits");
-  // pi.add_action("AnaHits", std::move(treeanalyzer));
+  topoproducer->verbose(true);
+  std::unique_ptr<PIAna::PITreeAnalyzer> treeanalyzer =
+      std::make_unique<PIAna::PITreeAnalyzer>("AnaHits");
+  pi.add_action("AnaHits", std::move(treeanalyzer));
   pi.add_action("AccAna", std::make_unique<PIAna::PIAccAnalyzer>("AccAna"));
   auto accana = dynamic_cast<PIAna::PIAccAnalyzer *>(pi.get_action("AccAna"));
   accana->topoflag_name("Topo_flag");
@@ -101,6 +102,6 @@ void run_test_pimue()
 
 void test_job()
 {
-  run_test_pienu();
+  // run_test_pienu();
   run_test_pimue();
 }
